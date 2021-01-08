9210aa4bd097760884f1cf147d58d6a12dbd87a7
Abbie Weisenbloom <Froggie@abbies-mbp.lan>
2021-01-07 23:13:38 -0800

    Added Actor and Movie Models

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
+#----------------------------------------------------------------------------#
+#  App Config.
+#----------------------------------------------------------------------------#
+
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
-
+# migrate = Migrate(app, db, compare_type=True)  -- what does this do
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
@@ -16,7 +20,12 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    #db.create_all()
+
+
+#----------------------------------------------------------------------------#
+# Models
+#----------------------------------------------------------------------------#
 
 
 '''
@@ -39,3 +48,47 @@ class Person(db.Model):
       'id': self.id,
       'name': self.name,
       'catchphrase': self.catchphrase}
+
+class Actor(db.Model):
+  __tablename__ = 'Actor'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  age = Column(Integer)
+  gender = Column(String)
+  movies = db.relationship('Movie', backref='Actor', lazy=True)
+
+  '''def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase'''
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'gender': self.gender}
+
+  def __repr__(self):
+    return f'<Actor ID: {self.id}, name: {self.name}, gender: {self.gender}>'
+
+class Movie(db.Model):
+  __tablename__ = 'Movie'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(DateTime)
+  artist_id = Column(Integer, db.ForeignKey('Actor.id'))
+
+
+  '''def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase'''
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.name,
+      'release_date': self.release_date}
+  
+  def __repr__(self):
+    return f'<Movie ID: {self.id}, title: {self.name}, release_date: {self.release_date}>'
